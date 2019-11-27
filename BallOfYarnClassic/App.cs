5f8162ed082d3c1241443e7d4bf91f51be5b315e
mlopes <mlopes@starbucks.com>
2019-11-27 12:10:03 -0800

    Adding more inputs and doing some cleanup

--- BallOfYarnClassic/App.cs
+++ BallOfYarnClassic/App.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using NewRelic.Api.Agent;
 
@@ -9,7 +10,7 @@ namespace BallOfYarnClassic
     {
         private bool shutdownInitiated = false;
 
-        private string lastKeyPress;
+        private string lastKeyPress = String.Empty;
 
         public void Run(string[] args)
         {
@@ -52,38 +53,15 @@ namespace BallOfYarnClassic
 
         public void DoLoop()
         {
-            Console.WriteLine("Press any key to exit.");
+            Console.WriteLine("Press x to exit.");
 
             int count = 1;
 
             while (!shutdownInitiated)
             {
-                this.DoStuff(count++);
+                this.SpinYarn(count++);
 
-                this.DoSomethingElse();
-
-                try
-                {
-                    if (lastKeyPress == "n")
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("Someone set us up the bomb.");
-                        throw new Exception("Detected an n bomb!");
-                    }
-
-                    if (lastKeyPress == "x")
-                    {
-                        shutdownInitiated = true;
-                    }                  
-                }
-                catch (Exception ex)
-                {
-                    NewRelic.Api.Agent.NewRelic.NoticeError(ex);
-                }
-                finally
-                {
-                    lastKeyPress = string.Empty;
-                }
+                this.HandleInput();
             }
 
             Console.WriteLine("Shutting down background process...");
@@ -92,13 +70,15 @@ namespace BallOfYarnClassic
 
         // This shows up as a top-level transaction.
         [Transaction]
-        public void DoStuff(int count)
-        {
-            Thread.Sleep(2500);
+        public void SpinYarn(int count)
+        {           
+            // There must be some time spent with the thread inside the decorated method,
+            // Otherwise you'll see a flat graph, which isn't great for demo purposes.
+            Thread.Sleep(1000); 
 
             this.WriteToConsole();
 
-            this.ApplyStuffCountAttribute(count);
+            ApplyCustomParametersToTransaction(count);
 
             NewRelic.Api.Agent.NewRelic.RecordCustomEvent(
                 "Stuff happens.",
@@ -107,48 +87,91 @@ namespace BallOfYarnClassic
 
         // This shows up as a top-level transaction.
         [Transaction]
-        public void DoSomethingElse()
+        public void HandleInput()
         {
-            Thread.Sleep(2500);
+            Thread.Sleep(1000);
+
+            try
+            {
+                if (lastKeyPress == "n")
+                {
+                    lastKeyPress = string.Empty;
+                    Thread.Sleep(500);
+                    Console.WriteLine("Someone set us up the bomb.");
+                    throw new IOException("Someone set us up the bomb.");
+                }
+
+                if (lastKeyPress == "b")
+                {
+                    lastKeyPress = string.Empty;
+                    Thread.Sleep(500);
+                    Console.WriteLine("All your base are belong to us.");
+                    throw new IOException("All your base are belong to us.");
+                }
+
+                if (lastKeyPress == "j")
+                {
+                    lastKeyPress = string.Empty;
+                    Console.WriteLine("For Great Justice!");
+                    var eventAttributes = new Dictionary<string, object> { { "Local Time:", DateTime.Now.ToLocalTime().ToShortDateString() } };
+                    NewRelic.Api.Agent.NewRelic.RecordCustomEvent("ForGreatJustice", eventAttributes);
+                }
+
+                if (lastKeyPress == "x")
+                {
+                    shutdownInitiated = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                // This API call ensures that the exception is metered even if swallowed
+                NewRelic.Api.Agent.NewRelic.NoticeError(ex);
+            }
 
             this.WriteToConsole();
 
             this.ChaosMetric();
         }
 
-        // This shows up in the breakdown of both DoStuff and DoSomethingElse, since they both invoke it.
+        // This method call shows up in the breakdown of both DoStuff and DoSomethingElse, since they both invoke it.
         [Trace]
         public void WriteToConsole()
         {
-            Console.WriteLine("Still running...");
+            //TODO: verify this works as expected
+            // Sleep added here so that we can observe a "time" in the breakdown
+
+            Thread.Sleep(500); 
+            Console.WriteLine("Spinning thread into yarn...");
         }
 
         // As a nested transaction, this doesn't show up in the Transactions table
         // However, it does appear in the breakdown under the DoSomethingElse transaction.
-        // TODO: determine what, if any, is the difference between a Trace and a nested transaction
+        // As documented by NewRelic
         [Transaction]
         public void ChaosMetric()
         {
+            //TODO: discover why this doesn't show up anywhere
             NewRelic.Api.Agent.NewRelic.RecordMetric("Chaos Factor", new Random().Next(0, 100));
         }
 
         // This line causes a metric to appear under the Attributes section of the DoStuff transaction.
-        // These metrics do NOT appear as transactions themselves in the high-level overview.
-        private void ApplyStuffCountAttribute(int count)
+        // These metrics do NOT appear as transactions themselves in the high-level overview, since there's no decoration
+        private void ApplyCustomParametersToTransaction(int count)
         {
-            
-            NewRelic.Api.Agent.NewRelic.AddCustomParameter("Stuff Count", count);
+            NewRelic.Api.Agent.NewRelic.AddCustomParameter("Iteration #:", count);
+            NewRelic.Api.Agent.NewRelic.AddCustomParameter("Details", "Yo dawg I herd you like custom parameters.");
         }
 
         // Have not been able to find any instances of this data in APM.
         // TODO: find out where custom events appear or how to correctly raise them
-        static void RaiseShutdownEvent()
+        //[Transaction]
+        void RaiseShutdownEvent()
         {
             var eventAttributes = new Dictionary<string, object> { { "Local Time:", DateTime.Now.ToLocalTime().ToShortDateString() } };
             NewRelic.Api.Agent.NewRelic.RecordCustomEvent("Application Shutdown.", eventAttributes);
         }
 
         // TODO: consider using Insights as the indicator of run completion, etc
-        // https://insights.newrelic.com/accounts/2116180/manage/api_keys
+        // https://insights.newrelic.com/accounts/2116180/explorer/events?eventType=Transaction&duration=1800000&facet=Stuff%20Count
     }
 }
